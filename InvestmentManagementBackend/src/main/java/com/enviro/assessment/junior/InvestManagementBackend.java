353d39d1d83da85a8b1247bee45cb110294bffeb
Bongani Moche <bonganimoche8@gmail.com>
2026-06-08 12:15:54 +0200

    chore: initial Spring Boot project setup

---
+++ InvestmentManagementBackend/src/main/java/com/enviro/assessment/junior/InvestManagementBackend.java
@@ -0,0 +1,11 @@
+package com.enviro.assessment.junior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestManagementBackend {
+    public static void main(String[] args) {
+        SpringApplication.run(InvestManagementBackend.class, args);
+    }
+}
